ea887146efca5be9adffacbf3c9c9170de2f7a13
PotapenkoPavel <57073819+PotapenkoPavel@users.noreply.github.com>
2020-08-30 00:00:21 +0300

    Add files via upload

---
+++ burger.js
@@ -0,0 +1,17 @@
+(function() {
+    let toggle = document.getElementById('menu__toggle');
+    let navbar = document.getElementById('navigation-burger')
+
+    console.log(navbar);
+    toggle.addEventListener('click', () => {
+        let checked = toggle.checked
+        document.body.style.overflow = checked ? 'hidden' : 'auto'
+        navbar.style.display = checked ? 'block' : 'none'
+    })
+
+    window.addEventListener('resize', () => {
+        toggle.checked = false
+        document.body.style.overflow = 'auto'
+        navbar.style.display = 'none'
+    })
+})()
